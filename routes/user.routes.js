16bd84e0d0d9a713832191ac05f9d744558ca75c
victudor09 <victudor09@gmail.com>
2024-10-19 13:39:24 +0200

    sab 13:39

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const UserController = require('../controllers/UserController');
 const { authentication, isAdmin } = require('../middlewares/authentication');
 
-router.get('/allprofiles', isAdmin, UserController.getAll); 
-router.get('/profile', authentication, UserController.getById); //CREAR EL ENDPOINT EN EL USER CONTROLLER
+router.get('/getall', isAdmin, UserController.getAll); 
+router.get('/userinfo', authentication, UserController.getAll); //CREAR EL ENDPOINT EN EL USER CONTROLLER
 router.post('/login', UserController.login);
 
 module.exports = router;
