28fe8ac1271bdcb8ac3aa5996a7e9a3786cc8551
chuans <juaazn.05@gmail.com>
2024-10-20 22:48:16 +0200

    Contantes

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,6 @@
 const COOKIE_OPTIONS = {
   httpOnly: true, // Solo accesible a través del servidor
   secure: process.env.NODE_ENV === 'production', // Cookies seguras solo en producción
-  maxAge: 24 * 60 * 60 * 1000 // Duración de un día
+  maxAge: 24 * 60 * 60 * 1000, // Duración de un día
+  SameSite: 'strict'
 };
