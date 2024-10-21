fdc871f59a068ac592c4a370e8003dc87cb32e6f
victudor09 <victudor09@gmail.com>
2024-10-21 13:54:33 +0200

    lunes 13:54 getOneUser

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,8 +4,8 @@ const UserController = require('../controllers/UserController');
 const { authentication, isAdmin } = require('../middlewares/authentication');
 
 router.get('/getall', isAdmin, UserController.getAll);
-router.get('/profile', UserController.getAll);
+router.get('/profile', UserController.getOneUser);
 router.put('/login', UserController.login);
-router.delete('/logout', authentication, UserController.logout); c2fd26d64b29e498ae496fb64b04f072a1a88bb8
+router.delete('/logout', authentication, UserController.logout);
 
 module.exports = router;
